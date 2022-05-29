0df3aea21aa2726ee6123b4acfe2d09008b3de43
Jerin S <jerinmanjadiyil@gmail.com>
2022-05-29 15:09:22 +0530

    face detection using webcam is done

--- facedetect.py
+++ facedetect.py
@@ -1,14 +1,16 @@
 import cv2
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+webcam = cv2.VideoCapture(0)
 
-img = cv2.imread('oscar.jpg')
-imS = cv2.resize(img, (960, 640))
-greyscale_img = cv2.cvtColor(imS,cv2.COLOR_BGR2GRAY)
-face_coordinates = trained_face_data.detectMultiScale(greyscale_img)
-#print(face_coordinates)
-for face in face_coordinates:
-    (x,y,w,h) = face
-    cv2.rectangle(imS,(x,y),(x+w , y+h ),(0,255,0),2)
-cv2.imshow('Actor',imS)
-cv2.waitKey()
-print('code completed')
+while True:
+    successful_read,frame = webcam.read()
+    greyscale_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    face_coordinates = trained_face_data.detectMultiScale(greyscale_img)
+    for face in face_coordinates:
+        (x,y,w,h) = face
+        cv2.rectangle(frame,(x,y),(x+w , y+h ),(0,255,0),2)
+    cv2.imshow('Face Detection',frame)
+    key =  cv2.waitKey(1)
+    if key == 81 or key == 113:#ASCII for q|Q
+        break
+print("Face detection successful!")
